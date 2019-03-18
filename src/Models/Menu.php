100b6c8967869490ecd8ef2396353ef294d5ec9b
Adem Kouki <ademkingnew@gmail.com>
2019-03-18 19:46:47 +0100

    Menu Url is now using APP_URL

--- src/Models/Menu.php
+++ src/Models/Menu.php
@@ -1,33 +1,26 @@
 <?php
-
 namespace TCG\Voyager\Models;
-
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
 use TCG\Voyager\Events\MenuDisplay;
 use TCG\Voyager\Facades\Voyager;
-
 /**
  * @todo: Refactor this class by using something like MenuBuilder Helper.
  */
 class Menu extends Model
 {
     protected $table = 'menus';
-
     protected $guarded = [];
-
     public function items()
     {
         return $this->hasMany(Voyager::modelClass('MenuItem'));
     }
-
     public function parent_items()
     {
         return $this->hasMany(Voyager::modelClass('MenuItem'))
             ->whereNull('parent_id');
     }
-
     /**
      * Display menu.
      *
@@ -47,23 +40,17 @@ class Menu extends Model
             }])
             ->first();
         });
-
         // Check for Menu Existence
         if (!isset($menu)) {
             return false;
         }
-
         event(new MenuDisplay($menu));
-
         // Convert options array into object
         $options = (object) $options;
-
         $items = $menu->parent_items->sortBy('order');
-
         if ($menuName == 'admin' && $type == '_json') {
             $items = static::processItems($items);
         }
-
         if ($type == 'admin') {
             $type = 'voyager::menu.'.$type;
         } else {
@@ -73,68 +60,55 @@ class Menu extends Model
                 $type = 'voyager::menu.bootstrap';
             }
         }
-
         if (!isset($options->locale)) {
             $options->locale = app()->getLocale();
         }
-
         if ($type === '_json') {
             return $items;
         }
-
         return new \Illuminate\Support\HtmlString(
             \Illuminate\Support\Facades\View::make($type, ['items' => $items, 'options' => $options])->render()
         );
     }
-
     public function save(array $options = [])
     {
         //Remove from cache
         \Cache::forget('voyager_menu_'.$this->name);
-
         parent::save();
     }
-
     private static function processItems($items)
     {
         $items = $items->transform(function ($item) {
             // Translate title
             $item->title = $item->getTranslatedAttribute('title');
             // Resolve URL/Route
-            $item->href = $item->link();
-
-            if (url($item->href) == url()->current() && $item->href != '') {
+            $item->href = $item->link(true);
+            if ($item->href == url()->current() && $item->href != '') {
                 // The current URL is exactly the URL of the menu-item
                 $item->active = true;
-            } elseif (starts_with(url()->current(), Str::finish(url($item->href), '/'))) {
+            } elseif (starts_with(url()->current(), Str::finish($item->href, '/'))) {
                 // The current URL is "below" the menu-item URL. For example "admin/posts/1/edit" => "admin/posts"
                 $item->active = true;
             }
-
-            if (($item->href == '' || url($item->href) == route('voyager.dashboard')) && $item->children->count() > 0) {
+            if (($item->href == url('') || $item->href == route('voyager.dashboard')) && $item->children->count() > 0) {
                 // Exclude sub-menus
                 $item->active = false;
-            } elseif (url($item->href) == route('voyager.dashboard') && url()->current() != route('voyager.dashboard')) {
+            } elseif ($item->href == route('voyager.dashboard') && url()->current() != route('voyager.dashboard')) {
                 // Exclude dashboard
                 $item->active = false;
             }
-
             if ($item->children->count() > 0) {
                 $item->setRelation('children', static::processItems($item->children));
-
                 if (!$item->children->where('active', true)->isEmpty()) {
                     $item->active = true;
                 }
             }
-
             return $item;
         });
-
         // Filter items by permission
         $items = $items->filter(function ($item) {
             return !$item->children->isEmpty() || app('VoyagerAuth')->user()->can('browse', $item);
         });
-
         return $items->values();
     }
 }
